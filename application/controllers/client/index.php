6a4b3caf051a674c593d64216fc412ed42678688
unktir <valera47304@gmail.com>
2023-02-28 13:51:40 +0500

    chore: finish client index controller

--- application/controllers/client/index.php
+++ application/controllers/client/index.php
@@ -2,10 +2,17 @@
 
 use core\App;
 use core\Database;
+use models\Driver;
 
 $db = App::resolve(Database::class);
+$driver_class = new Driver();
+
+$car_id = 2;
+
+$car_offenses = $driver_class->getCarOffensesById($car_id);
 
 view('layouts/default.php', [
     'type' => 'client',
-    'title' => 'Оплата штрафов'
+    'title' => 'Оплата штрафов',
+    'car_offenses' => $car_offenses,
 ]);
