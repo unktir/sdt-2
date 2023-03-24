ce50814f7c3e06f32076d0b47d6670189c06ba97
unktir <valera47304@gmail.com>
2023-03-24 14:47:48 +0500

    feat(header): add exit button display

--- application/views/client/templates/header.php
+++ application/views/client/templates/header.php
@@ -4,9 +4,11 @@
             <a class="button" href="/admin">
                 Страница администратора
             </a>
-            <div class="functional">
-                <button class="button" id="exit" type="button">Выйти</button>
-            </div>
+            <?php if ($_SESSION['user'] ?? false) : ?>
+                <div class="functional">
+                    <button class="button" id="exit" type="button">Выйти</button>
+                </div>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
