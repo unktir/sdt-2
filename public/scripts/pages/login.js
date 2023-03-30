0a358138846a67234738665380f86fa85560c287
unktir <valera47304@gmail.com>
2023-03-30 16:04:39 +0500

    feat(login): rewrite validation test_gov_reg_num and login script

--- public/scripts/pages/login.js
+++ public/scripts/pages/login.js
@@ -1,20 +1,45 @@
 import {pop_up_message} from '../templates/pop_up_message.js';
+import {test_reg_num, test_reg_reg} from "../templates/test_gov_reg_num.js";
+
+//
+document.getElementById('registered_number').onchange = function (e) {
+    let result = test_reg_num(e.target)
+
+    if (result)
+        $(this).removeClass('error');
+    else
+        $(this).addClass('error');
+};
+
+//
+document.getElementById('registered_region').onchange = function (e) {
+    let result = test_reg_reg(e.target);
+
+    if (result)
+        $(this).removeClass('error');
+    else
+        $(this).addClass('error');
+};
 
 // Вход
 $('#sign-in-form').on('submit', function (e) {
     e.preventDefault();
 
-    $.ajax({
-        url: '/session',
-        method: 'POST',
-        dataType: 'html',
-        data: $(this).serialize(),
-        success: function () {
-            pop_up_message('Вы вошли!');
-            window.location = '/'
-        },
-        error: function () {
-            pop_up_message('Ошибка отправки! Попробуйте повторить позже')
-        }
-    });
+    if (!$('input', this).hasClass('error')) {
+        $.ajax({
+            url: '/session',
+            method: 'POST',
+            dataType: 'html',
+            data: $(this).serialize(),
+            success: function () {
+                pop_up_message('Вы вошли!');
+                //window.location = '/'
+            },
+            error: function () {
+                pop_up_message('Ошибка отправки! Попробуйте повторить позже')
+            }
+        });
+    } else {
+        pop_up_message('Введите корректный гос. рег. номер!')
+    }
 });
--- public/scripts/templates/test_gov_reg_num.js
+++ public/scripts/templates/test_gov_reg_num.js
@@ -1,28 +1,24 @@
-// Регулярное выражение для гос. рег. номера
+// Регулярные выражения для гос. рег. номера
+// Регулярные выражения для номера
 let reg_num_regexp = /^[АВЕКМНОРСТУХавекмнорстух]{1}[0-9]{3}(?<!0{3})[АВЕКМНОРСТУХавекмнорстух]{2}$/u;
+// Регулярные выражения для региона
 let reg_reg_regexp = /\d{2,3}$/u;
 
-// Функция для проверки гос. рег. номера
-export function test_gov_reg_num(input_reg_reg, input_reg_num) {
-    if (!((input_reg_num.value == null) && (input_reg_reg.value == null))) {
-        let OK_reg_num = reg_num_regexp.exec(input_reg_reg.value);
-        let OK_reg_reg = reg_reg_regexp.exec(input_reg_num.value);
+// Функции для проверки гос. рег. номера
+// Функция для проверки номера
+export function test_reg_num(input_reg_num) {
+    if (!(input_reg_num.value == null)) {
+        let OK_reg_num = reg_num_regexp.exec(input_reg_num.value);
 
-        if (OK_reg_num)
-            $('#registered_number').removeClass('error');
-        else
-            $('#registered_number').addClass('error');
-        if (OK_reg_reg)
-            $('#registered_region').removeClass('error');
-        else
-            $('#registered_region').addClass('error');
+        return !!OK_reg_num;
+    }
+}
 
-        if (OK_reg_num && OK_reg_reg) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
+// Функция для проверки региона
+export function test_reg_reg(input_reg_reg) {
+    if (!(input_reg_reg.value == null)) {
+        let OK_reg_reg = reg_reg_regexp.exec(input_reg_reg.value);
+
+        return !!OK_reg_reg;
     }
 }
