f37b8770d621c1ae6b7699ce7a06fba78837c130
unktir <valera47304@gmail.com>
2023-04-01 14:04:11 +0500

    refactor(admin): rewrite almost all scripts for better work

--- public/scripts/pages/admin.js
+++ public/scripts/pages/admin.js
@@ -1,25 +1,105 @@
 import {pop_up_message} from '../templates/pop_up_message.js';
 import {arrow_swap} from '../templates/arrow_swap.js';
-import {test_full_name} from '../templates/test_full_name.js';
-import {test_gov_reg_num} from '../templates/test_gov_reg_num.js';
+import {test_string} from '../templates/test_full_name.js';
+import {test_reg_num, test_reg_reg} from '../templates/test_gov_reg_num.js';
 
-// Функция создания списка автомобилей водителя с помощью ФИО
-function get_car_list_by_full_name() {
-    let car_select = $('#car_id');
-    let fine_form = document.forms.fine_form;
-    let first_name = fine_form.elements.pay_first_name.value;
-    let middle_name = fine_form.elements.pay_middle_name.value;
-    let last_name = fine_form.elements.pay_last_name.value;
+// Секция формы добавления водителя и/или автомобиля
+$(document).ready(function () {
+    // Валидация ФИО
+    $('.driver-form input', '#add-the-driver-form').on('change', function (e) {
+        let result = test_string(e.target.value);
 
-    if (!(first_name == null || first_name === "") && !(middle_name == null || middle_name === "") && !(last_name == null || last_name === "")) {
+        if (result)
+            $(this).removeClass('error');
+        else
+            $(this).addClass('error');
+    });
+
+    // Валидация рег. номера
+    $('#registered_number').on('change', function (e) {
+        let result = test_reg_num(e.target.value);
+
+        if (result)
+            $(this).removeClass('error');
+        else
+            $(this).addClass('error');
+    });
+
+    // Валидация рег. региона
+    $('#registered_region').on('change', function (e) {
+        let result = test_reg_reg(e.target.value);
+
+        if (result)
+            $(this).removeClass('error');
+        else
+            $(this).addClass('error');
+    });
+
+    // Добавление данных формы в базу данных
+    $('#add-the-driver-form').on('submit', function (e) {
+        e.preventDefault();
+        let full_name_flag = $('.driver-form input', this).hasClass('error');
+        let gov_reg_num_flag = $('.car-number-and-name-form input', this).hasClass('error');
+
+        if (gov_reg_num_flag || full_name_flag) {
+            return;
+        }
+
+        $.ajax({
+            url: '/admin/storeDriver',
+            method: 'POST',
+            dataType: 'html',
+            data: $(this).serialize(),
+            success: function (message) {
+                document.getElementById('add-the-fine-form').reset();
+                pop_up_message(message);
+            },
+            error: function () {
+                pop_up_message('Ошибка отправки! Попробуйте повторить позже')
+            }
+        });
+    });
+});
+
+// Секция формы добавления штрафа
+$(document).ready(function () {
+    // Получение списка автомобилей по ФИО
+    $('.fine-driver-form input', '#add-the-fine-form').on('change', function (e) {
+        let result = test_string(e.target.value);
+        let car_select = $('#car_id');
+
+        if (!result) {
+            $(this).addClass('error');
+            car_select.prop('disabled', true);
+        } else {
+            $(this).removeClass('error');
+        }
+
+        let full_name = $('.fine-driver-form input', '#add-the-fine-form').map(function () {
+            return $(this).val();
+        });
+        let full_name_empty_flag = full_name.is(function () {
+            return (this === '');
+        });
+
+        if (full_name_empty_flag) {
+            return;
+        }
+
+        let full_name_error_flag = $('.fine-driver-form input', '#add-the-fine-form').hasClass('error');
+
+        if (full_name_error_flag) {
+            return;
+        }
+
         $.ajax({
             url: '/admin/getCarList',
             method: 'GET',
             dataType: 'html',
             data: {
-                first_name: first_name,
-                middle_name: middle_name,
-                last_name: last_name
+                last_name: full_name[0],
+                first_name: full_name[1],
+                middle_name: full_name[2]
             },
             success: function (option) {
                 car_select.html(option);
@@ -29,120 +109,55 @@ function get_car_list_by_full_name() {
                 pop_up_message('Ошибка отправки! Попробуйте повторить позже')
             }
         });
-    } else {
-        car_select.prop('disabled', true);
-    }
-}
-
-// Функция создания списка статей с помощью Раздела
-function get_article_by_chapter_id() {
-    let chapter_select = $('#chapter_id');
-    let article_select = $('#offense_id');
-
-    $.ajax({
-        url: '/admin/getOffensesArticle',
-        method: 'GET',
-        dataType: 'html',
-        data: {
-            chapter_id: chapter_select.val()
-        },
-        success: function (option) {
-            article_select.html(option);
-            article_select.prop('disabled', false);
-        },
-        error: function () {
-            pop_up_message('Ошибка отправки! Попробуйте повторить позже')
-        }
-    });
-}
-
-// Функция создания описания нарушения с помощью Статьи
-function get_description_by_article_id() {
-    let chapter_select = $('#chapter_id')
-    let article_select = $('#offense_id');
-
-    $('.offence-article-description-wrap-block').show();
-    $.ajax({
-        url: '/admin/findDescription',
-        method: 'GET',
-        dataType: 'html',
-        data: {
-            id: article_select.val(),
-        },
-        success: function (description) {
-            $('.offence-article-description-block').html(description);
-        },
-        error: function () {
-            pop_up_message('Ошибка отправки! Попробуйте повторить позже')
-        }
     });
 
-    chapter_select.change(function () {
-        $('.offence-article-description-wrap-block').hide();
-    });
-}
+    // Получение статьи по разделу
+    document.getElementById('chapter_id').onchange = function () {
+        let chapter_select = $(this);
+        let article_select = $('#offense_id');
+        let oadwb = $('.offence-article-description-wrap-block');
 
-//
-$(document).ready(function () {
-    // Добавление данных о водителе в базу данных
-    $('#add-the-driver-form').on('submit', function (e) {
-        e.preventDefault();
-        let gov_reg_num_flag = test_gov_reg_num(document.getElementById('registered_number'), document.getElementById('registered_region'));
-        let full_name_flag = test_full_name(document.getElementById('add_first_name'), document.getElementById('add_middle_name'), document.getElementById('add_last_name'));
-        if (gov_reg_num_flag && full_name_flag) {
-            $.ajax({
-                url: '/admin/storeDriver',
-                method: 'POST',
-                dataType: 'html',
-                data: $(this).serialize(),
-                success: function (message) {
-                    document.getElementById('add-the-fine-form').reset();
-                    pop_up_message(message);
-                },
-                error: function () {
-                    pop_up_message('Ошибка отправки! Попробуйте повторить позже')
-                }
-            });
-        }
-    });
+        oadwb.hide();
 
-    // Добавление данных о штрафе в базу данных
-    $('#add-the-fine-form').on('submit', function (e) {
-        e.preventDefault();
         $.ajax({
-            url: '/admin/storeOffense',
-            method: 'POST',
+            url: '/admin/getOffensesArticle',
+            method: 'GET',
             dataType: 'html',
-            data: $(this).serialize(),
-            success: function (message) {
-                // document.getElementById('add-the-fine-form').reset();
-                pop_up_message(message);
+            data: {
+                chapter_id: chapter_select.val()
+            },
+            success: function (option) {
+                article_select.html(option);
+                article_select.prop('disabled', false);
             },
             error: function () {
                 pop_up_message('Ошибка отправки! Попробуйте повторить позже')
             }
         });
-    });
-
-    // При изменение ФИО вызывается функция get_car_list_by_full_name()
-    document.getElementById('pay_first_name').onchange = function () {
-        get_car_list_by_full_name()
-    };
-    document.getElementById('pay_middle_name').onchange = function () {
-        get_car_list_by_full_name()
-    };
-    document.getElementById('pay_last_name').onchange = function () {
-        get_car_list_by_full_name()
     };
 
-    // При изменение раздела вызывается функция get_article_by_chapter_id()
-    document.getElementById('chapter_id').onchange = function () {
-        get_article_by_chapter_id()
-    };
-
-    // При изменение статьи вызывается функция get_description_by_article_id()
+    // Получение описания по статьи
     document.getElementById('offense_id').onchange = function () {
-        get_description_by_article_id()
+        let article_select = $(this);
+        let oadwb = $('.offence-article-description-wrap-block');
+        let oadb = $('.offence-article-description-block');
+
+        oadwb.show();
+
+        $.ajax({
+            url: '/admin/findDescription',
+            method: 'GET',
+            dataType: 'html',
+            data: {
+                id: article_select.val(),
+            },
+            success: function (description) {
+                oadb.html(description);
+            },
+            error: function () {
+                pop_up_message('Ошибка отправки! Попробуйте повторить позже')
+            }
+        });
     };
 
     // Раскрытие и закрытие гармошки описания штрафов
@@ -161,5 +176,29 @@ $(document).ready(function () {
                 oadw.removeClass('open');
             }, 400);
         }
+    });
+
+    // Добавление данных формы в базу данных
+    $('#add-the-fine-form').on('submit', function (e) {
+        e.preventDefault();
+        let full_name_error_flag = $('.fine-driver-form input', this).hasClass('error');
+
+        if (full_name_error_flag) {
+            return;
+        }
+
+        $.ajax({
+            url: '/admin/storeOffense',
+            method: 'POST',
+            dataType: 'html',
+            data: $(this).serialize(),
+            success: function (message) {
+                // document.getElementById('add-the-fine-form').reset();
+                pop_up_message(message);
+            },
+            error: function () {
+                pop_up_message('Ошибка отправки! Попробуйте повторить позже')
+            }
+        });
     });
 });
