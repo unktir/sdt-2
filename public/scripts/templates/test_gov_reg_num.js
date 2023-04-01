eda494ca8de9676d95dbdb8b216fc9bab8e431b8
unktir <valera47304@gmail.com>
2023-04-01 13:54:33 +0500

    fix(test_gov_reg_num): change function parameters

--- public/scripts/templates/test_gov_reg_num.js
+++ public/scripts/templates/test_gov_reg_num.js
@@ -1,23 +1,23 @@
 // Регулярные выражения для гос. рег. номера
 // Регулярные выражения для номера
-let reg_num_regexp = /^[АВЕКМНОРСТУХавекмнорстух]{1}[0-9]{3}(?<!0{3})[АВЕКМНОРСТУХавекмнорстух]{2}$/u;
+let reg_num_regexp = /^[АВЕКМНОРСТУХавекмнорстух][0-9]{3}(?<!0{3})[АВЕКМНОРСТУХавекмнорстух]{2}$/u;
 // Регулярные выражения для региона
 let reg_reg_regexp = /\d{2,3}$/u;
 
 // Функции для проверки гос. рег. номера
 // Функция для проверки номера
-export function test_reg_num(input_reg_num) {
-    if (!(input_reg_num.value == null)) {
-        let OK_reg_num = reg_num_regexp.exec(input_reg_num.value);
+export function test_reg_num(reg_num_value) {
+    if (!(reg_num_value == null)) {
+        let OK_reg_num = reg_num_regexp.exec(reg_num_value);
 
         return !!OK_reg_num;
     }
 }
 
 // Функция для проверки региона
-export function test_reg_reg(input_reg_reg) {
-    if (!(input_reg_reg.value == null)) {
-        let OK_reg_reg = reg_reg_regexp.exec(input_reg_reg.value);
+export function test_reg_reg(reg_reg_value) {
+    if (!(reg_reg_value == null)) {
+        let OK_reg_reg = reg_reg_regexp.exec(reg_reg_value);
 
         return !!OK_reg_reg;
     }
