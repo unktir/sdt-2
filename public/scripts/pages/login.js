515cf9230e3924724a8bba934c1de3aa84eb64f0
unktir <valera47304@gmail.com>
2023-04-01 14:22:48 +0500

    refactor(login): change sign in and validation scripts

--- public/scripts/pages/login.js
+++ public/scripts/pages/login.js
@@ -1,45 +1,47 @@
 import {pop_up_message} from '../templates/pop_up_message.js';
 import {test_reg_num, test_reg_reg} from "../templates/validator.js";
 
-//
-document.getElementById('registered_number').onchange = function (e) {
+// Валидация рег. номера
+$('#registered_number').on('change', function (e) {
     let result = test_reg_num(e.target.value);
 
     if (result)
         $(this).removeClass('error');
     else
         $(this).addClass('error');
-};
+});
 
-//
-document.getElementById('registered_region').onchange = function (e) {
+// Валидация рег. региона
+$('#registered_region').on('change', function (e) {
     let result = test_reg_reg(e.target.value);
 
     if (result)
         $(this).removeClass('error');
     else
         $(this).addClass('error');
-};
+});
 
 // Вход
 $('#sign-in-form').on('submit', function (e) {
     e.preventDefault();
+    let gov_reg_num_flag = $('input', this).hasClass('error');
 
-    if (!$('input', this).hasClass('error')) {
-        $.ajax({
-            url: '/session',
-            method: 'POST',
-            dataType: 'html',
-            data: $(this).serialize(),
-            success: function () {
-                pop_up_message('Вы вошли!');
-                window.location = '/';
-            },
-            error: function () {
-                pop_up_message('Ошибка отправки! Попробуйте повторить позже')
-            }
-        });
-    } else {
+    if (gov_reg_num_flag) {
         pop_up_message('Введите корректный гос. рег. номер!')
+        return;
     }
+
+    $.ajax({
+        url: '/session',
+        method: 'POST',
+        dataType: 'html',
+        data: $(this).serialize(),
+        success: function () {
+            pop_up_message('Вы вошли!');
+            window.location = '/';
+        },
+        error: function () {
+            pop_up_message('Ошибка отправки! Попробуйте повторить позже')
+        }
+    });
 });
