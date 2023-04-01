4b2a2b93969a3066ae23603b86a537573ca70d79
unktir <valera47304@gmail.com>
2023-04-01 13:57:38 +0500

    fix(test_full_name): fix regexp and function behaviour

--- public/scripts/templates/test_full_name.js
+++ public/scripts/templates/test_full_name.js
@@ -1,32 +1,11 @@
-// Регулярное выражение для ФИО
-let full_name_regexp = /^[а-яё]/iu;
+// Регулярное выражение для русского языка
+let rus_string_regexp = /^[А-ЯЁ][а-яё]*$/u;
 
-// Функция для проверки ФИО
-export function test_full_name(input_first_name, input_middle_name, input_last_name) {
-    if (!((input_first_name.value == null) && (input_middle_name.value == null) && (input_last_name.value == null))) {
-        let OK_first_name = full_name_regexp.exec(input_first_name.value);
-        let OK_middle_name = full_name_regexp.exec(input_middle_name.value);
-        let OK_last_name = full_name_regexp.exec(input_last_name.value);
+// Функция для проверки строки
+export function test_string(string_value) {
+    if (!(string_value == null)) {
+        let OK_input_string = rus_string_regexp.test(string_value);
 
-        if (OK_first_name)
-            $('#add_first_name').removeClass('error');
-        else
-            $('#add_first_name').addClass('error');
-        if (OK_middle_name)
-            $('#add_middle_name').removeClass('error');
-        else
-            $('#add_middle_name').addClass('error');
-        if (OK_last_name)
-            $('#add_last_name').removeClass('error');
-        else
-            $('#add_last_name').addClass('error');
-
-        if (OK_first_name && OK_middle_name && OK_last_name) {
-            return true;
-        } else {
-            return false;
-        }
-    } else {
-        return false;
+        return !!OK_input_string;
     }
 }
