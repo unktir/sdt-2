029a6bfe0293e72b8ce83b5e6aca07e6e85e2821
unktir <valera47304@gmail.com>
2023-04-19 21:27:17 +0500

    refactor(session): refactor session page and style

--- application/views/client/pages/session/create.php
+++ application/views/client/pages/session/create.php
@@ -1,16 +1,20 @@
 <main>
     <div class="container">
         <section class="authorisation-block content">
-            <h1>Авторизация</h1>
+            <h1>Проверка и оплата штрафов ГИБДД</h1>
             <form id="sign-in-form" name="sign-in-form">
                 <fieldset>
-                    <label id="car_number">Государственный регистрационный номер:</label>
-                    <input required id="registered_number" name="registered_number" aria-labelledby="car_number"
-                           type="text" placeholder="А000АА" maxlength="6">
-                    <input required id="registered_region" name="registered_region" aria-labelledby="car_number"
-                           type="text" placeholder="777" maxlength="3">
+                    <label id="car_number">Гос. рег. номер:</label>
+                    <div class="car-number-input">
+                        <input required id="registered_number" name="registered_number"
+                               aria-labelledby="car_number"
+                               type="text" placeholder="А000АА" maxlength="6">
+                        <input required id="registered_region" name="registered_region"
+                               aria-labelledby="car_number"
+                               type="text" placeholder="777" maxlength="3">
+                    </div>
                 </fieldset>
-                <button class="button sign-in">Войти</button>
+                <button class="button sign-in">Проверить штрафы</button>
             </form>
         </section>
     </div>
